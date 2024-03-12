e98c463d10112a6df4e6b8a72c727332c2ebc597
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.com>
2024-03-12 09:45:11 -0400

    chore: cleanup main header & sidebar

--- src/app/[locale]/search/page.tsx
+++ src/app/[locale]/search/page.tsx
@@ -3,22 +3,6 @@ import { Hits } from '@/components/hits';
 import { QuestionsResult } from '@/components/questions';
 import { SearchProvider } from '@/components/search-provider';
 import { SearchBox } from '@/components/searchbox';
-import { SearchRequest } from '@/lib/client';
-import { Star } from 'lucide-react';
-
-const parseSearchRequest = (searchParams: {
-  [key: string]: string | string[] | undefined;
-}): SearchRequest => {
-  const req: SearchRequest = {
-    query: '',
-  };
-
-  if (typeof searchParams.q === 'string') {
-    req.query = searchParams.q;
-  }
-
-  return req;
-};
 
 export default async function Search({
   searchParams,
@@ -26,14 +10,12 @@ export default async function Search({
   params: { locale: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const req = parseSearchRequest(searchParams);
-
   return (
     <SearchProvider>
       <div className="min-w-96 flex-grow">
         <div className="relative">
           <header className="absolute left-0 z-40 h-[48px] w-full border-b bg-background">
-            <Star />
+            {/* <Star /> */}
           </header>
           <div className="absolute left-0 h-screen w-full overflow-y-auto pt-[48px]">
             <div className="flex flex-col items-center justify-center gap-8 py-8">
--- src/app/[locale]/sidebar.tsx
+++ src/app/[locale]/sidebar.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { CliniaIcon } from '@clinia-ui/icons';
-import { Button } from '@clinia-ui/react';
 
 export const Sidebar = () => {
+  const router = useRouter();
   return (
     <aside className="fixed left-0 top-0 z-40 h-screen w-56 -translate-x-full border-r transition-transform sm:translate-x-0">
       <div className="h-full overflow-y-auto px-3 py-4">
-        <Button variant="link">
-          <Link href="/">
-            <CliniaIcon />
-          </Link>
-        </Button>
+        <div className="cursor-pointer p-2" onClick={() => router.push('/')}>
+          <CliniaIcon />
+        </div>
       </div>
     </aside>
   );
