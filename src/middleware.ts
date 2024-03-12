ba64f908d5b46e5c3826aabfcd9f45ca0045c1ca
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.com>
2024-03-12 07:59:20 -0400

    fix: probes not using intl

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { withMiddlewareAuthRequired } from '@/lib/auth/edge';
 import createMiddleware from 'next-intl/middleware';
 import { NextFetchEvent, NextRequest } from 'next/server';
 
@@ -7,18 +8,18 @@ const intlMiddleware = createMiddleware({
   localePrefix: 'never',
 });
 
-export default function middleware(req: NextRequest, _event: NextFetchEvent) {
-  // // We skip the next-intl middleware for the auth page as it is not translated
-  // // and the locale parameter is conflicting with the auth0 route handlers.
-  // // As the auth0 route handlers are not translated, we can safely skip it.
-  // const isAuthPage = req.nextUrl.pathname.startsWith('/auth');
+export default function middleware(req: NextRequest, event: NextFetchEvent) {
+  // We skip the next-intl middleware for the auth page as it is not translated
+  // and the locale parameter is conflicting with the auth0 route handlers.
+  // As the auth0 route handlers are not translated, we can safely skip it.
+  const isAuthPage = req.nextUrl.pathname.startsWith('/auth');
 
-  // if (isAuthPage) {
-  //   return withMiddlewareAuthRequired()(req, event);
-  // }
-  return intlMiddleware(req);
+  if (isAuthPage) {
+    return withMiddlewareAuthRequired()(req, event);
+  }
+  return withMiddlewareAuthRequired(intlMiddleware)(req, event);
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|livez|healthz|_vercel|.*\\..*).*)'],
 };
