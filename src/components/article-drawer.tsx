79ed3dc1a93e6f3af554839b8d0f5db9392a2705
William Bergeron-Drouin <william.bergeron-drouin@clinia.ca>
2024-09-09 10:30:46 -0400

    Adapt article drawer

--- src/components/article-drawer.tsx
+++ src/components/article-drawer.tsx
@@ -1,14 +1,35 @@
 'use client';
 
+import { getHighlightText } from '@/lib/client/util';
 import { X } from 'lucide-react';
-import Markdown from 'react-markdown';
-import Link from 'next/link';
+import { useMemo } from 'react';
 import { Button } from '@clinia-ui/react';
-import { PassageHighlight } from './highlight';
+import { HtmlDisplay } from './html-display';
 import { useSearchLayout } from './search-layout';
 
 export const ArticleDrawer = () => {
   const searchLayout = useSearchLayout();
+
+  const hitsToDisplay = useMemo((): string[] => {
+    const allhighlights = Object.values(
+      searchLayout.hit?.highlighting ?? {}
+    ).flat();
+    if (allhighlights.length === 0) {
+      return [];
+    }
+
+    const hitsHighlights = allhighlights.filter(
+      (highlight) => 'type' in highlight && highlight.type === 'hits'
+    );
+    if (hitsHighlights.length === 0) {
+      return allhighlights.map(getHighlightText);
+    }
+
+    // We display the hits by score
+    return hitsHighlights
+      .sort((a, b) => b.score - a.score)
+      .map(getHighlightText);
+  }, [searchLayout.hit?.highlighting]);
 
   if (!searchLayout.hit) {
     return null;
@@ -32,46 +53,17 @@ export const ArticleDrawer = () => {
       </header>
       <div className="absolute left-0 h-[calc(100vh-48px)] w-full overflow-y-auto pt-[48px]">
         <div className="px-4 py-6">
-          {/* TODO: Loop over hit.hihglingting of type hits and render them
-            We will sanitize the html and render it as html
-          */}
           <h1 className="mb-4 text-xl font-medium text-foreground">
             {searchLayout.hit.resource.data.title}
           </h1>
-          {/* TODO: remove me */}
-          <h2 className="text-lg">Abstract</h2>
-          <p className="text-sm">{searchLayout.hit.resource.data.abstract}</p>
 
-          {/* TODO: remove me */}
-          {searchLayout.hit.resource.data.content.map((content) => (
-            <div key={content.title}>
-              <h2 className="text-lg">{content.title}</h2>
-              <p className="text-sm">{content.text}</p>
-            </div>
-          ))}
-
-          {/* {searchLayout.hit.highlight.map((highlight, idx) => (
-            <Markdown
-              components={{
-                h1: (props) => (
-                  <h1
-                    className="mb-3 text-xl font-medium text-foreground"
-                    {...props}
-                  />
-                ),
-                h2: (props) => (
-                  <h2 className="mb-2 text-lg font-medium" {...props} />
-                ),
-                h3: (props) => (
-                  <h3 className="mb-1 text-base font-medium" {...props} />
-                ),
-              }}
+          {hitsToDisplay.map((highlight, idx) => (
+            <HtmlDisplay
+              className="my-4 whitespace-pre-line text-sm [&_em]:font-bold"
               key={idx}
-            >
-              {highlight.match}
-            </Markdown>
-            // <PassageHighlight key={idx} highlight={highlight} />
-          ))} */}
+              html={highlight}
+            />
+          ))}
         </div>
       </div>
     </div>
